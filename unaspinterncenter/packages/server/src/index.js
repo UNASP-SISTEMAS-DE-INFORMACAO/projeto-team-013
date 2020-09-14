dcc08d020cf9b9ee202c1dfa74a30cce05206a65
Carlos Henrique <carlosh.mb@outlook.com>
2020-09-14 20:00:41 -0300

    swagger documentation created

--- unaspinterncenter/packages/server/src/index.js
+++ unaspinterncenter/packages/server/src/index.js
@@ -1,7 +1,13 @@
 require('dotenv').config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 })
+
+const swaggerUi = require('swagger-ui-express')
+
 const server = require('./server.js')
+const swaggerOptions = require('./config/swaggerConfig')
+
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions))
 
 server.listen(3000)
 
