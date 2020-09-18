a27ec23e0c32be8e20bb5ce8a94bfc2bde088ca8
Carlos Henrique <48557266+CarlosHenr1que@users.noreply.github.com>
2020-09-18 19:21:17 -0300

    Task/04 (#7)
    * created register screen
    
    * register created
    
    * axios configuration created
    
    * created screen Confirmpassword and login
    
    Co-authored-by: Alex Genuino <genuinoalexsander@gmail.com>

---
+++ unaspinterncenter/packages/mobile/src/components/Input/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import { Container } from './styles';
+
+const Input = ({placeholder}) => {
+  return(
+  
+  <Container placeholder={placeholder}>
+
+  </Container>
+  
+  );
+}
+
+export default Input;
---
+++ unaspinterncenter/packages/mobile/src/pages/ConfirmPassword/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Buttom from "../../components/Buttom";
+import Input from "../../components/Input";
+import BackButtom from "../../components/BackButtom";
+import {
+  Container,
+  Logo,
+  Header,
+  Text,
+  Form,
+  ProgressContainer,
+  Progress,
+  InputContainer,
+} from "./styles";
+
+
+const ConfirmPassword = ({ navigation }) => {
+  const [progress, setProgress] = useState([
+    { progress: false },
+    { progress: false },
+    { progress: true },
+  ]);
+  return (
+    <Container>
+      <Header>
+        <BackButtom />
+        <Logo source={require("../../assets/unasp.png")} />
+        <Text>
+          Digite sua senha e confirme para finalizar o cadastro
+        </Text>
+        <ProgressContainer>
+          {progress.map((item) => (
+            <Progress status={item.progress} />
+          ))}
+        </ProgressContainer>
+      </Header>
+      <Form>
+            <InputContainer>
+                <Input placeholder="Digite sua senha"/>
+                <Input placeholder="Confirme sua senha"/>   
+            </InputContainer>
+        <Buttom tittle={"Finalizar cadastro"} />
+      </Form>
+    </Container>
+  );
+};
+
+export default ConfirmPassword;
